55e99cb17f9e6c0d939302ba309ca1d81d11a133
Reid Meng <jiashiwm@gmail.com>
2026-01-16 14:37:12 -0800

    scrap worked

--- lab1/JiashiwenMeng_8060740828/scripts/web_scraper.py
+++ lab1/JiashiwenMeng_8060740828/scripts/web_scraper.py
@@ -3,7 +3,11 @@ from bs4 import BeautifulSoup
 
 def scrape_website(url):
     try:
-        response = requests.get(url)
+        headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9",
+        }
+        response = requests.get(url, headers=headers, timeout=20)
         response.raise_for_status()
 
         soup = BeautifulSoup(response.text, 'html.parser')
